1bf45a1048d4b24e10dc9e2550ec34599c558e75
Jürgen Eidt <jurgene@microsoft.com>
2024-10-13 16:39:34 -0700

    update test case

--- unit_test_ev/unit_test_ev.cpp
+++ unit_test_ev/unit_test_ev.cpp
@@ -19,15 +19,23 @@ namespace unittest
 #pragma warning (push)
 #pragma warning(disable : 4996)
 
+			// int data
 			vector <unsigned int> matchList = { 1, 2, 3 };
 
 			// toString lambda expression
 			auto getText([](auto id) { CString text; text.Format(L"%d", id); return text; });
 
 			const CString matchParameter(JoinString<unsigned int>(matchList.begin(), matchList.end(), getText, L"(", L")", L", "));
-			
+
 			Assert::AreEqual(L"(1, 2, 3)", matchParameter);
 
+
+			// CString data
+			vector <CString> strList = { L"a", L"b", L"c" };
+
+			const CString combinedText(JoinString<CString>(strList.begin(), strList.end(), [](auto text) { return text; }, L"[", L"]", L"+"));
+
+			Assert::AreEqual(L"[a+b+c]", combinedText);
 
 #pragma warning (pop)
 		}
