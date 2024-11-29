bfb2cb182f46b8222806693ebd3cd29980e6d23c
Jürgen Eidt <jurgene@microsoft.com>
2024-11-29 13:29:30 -0800

    .

--- fp_ascii_art/resource.h
+++ fp_ascii_art/resource.h
@@ -31,7 +31,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        110
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1020
+#define _APS_NEXT_CONTROL_VALUE         1014
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
