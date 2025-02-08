663fb3e62bbb2bbdb6632b8795a35ddd1daf1eee
test <test@test.com>
2025-02-08 15:50:05 -0800

    .

--- cs_test/Class1.cs
+++ cs_test/Class1.cs
@@ -1,7 +1,10 @@
-﻿namespace cs_test
+﻿using System.Runtime.InteropServices;
+
+namespace cs_test
 {
     public class Class1
     {
-
+        [UnmanagedCallersOnly(EntryPoint = nameof(TestMethod))]
+        public static void TestMethod() => Console.Out.WriteLine("Hello from C#");
     }
 }
