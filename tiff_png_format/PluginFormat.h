7366f7e1e3137bab3ea8b4b421b99421a1ce4c5b
test <test@test.com>
2025-03-15 21:40:08 -0700

    add tiff_png

---
+++ tiff_png_format/PluginFormat.h
@@ -0,0 +1,125 @@
+#pragma once
+#include "pictureformat.h"
+
+
+class CPluginFormat : public CPictureFormat
+{
+protected:
+	CPluginFormat();
+
+public:
+	virtual ~CPluginFormat() {};
+
+protected:
+	CStringA get_utf8_file_name(const CString& FileName);
+
+public:
+	virtual bool __stdcall RGBToFile(const CString& FileName,
+		const BYTE* dataBuf,
+		const int width,
+		const int height,
+		const int quality_L = -1,
+		const int quality_C = -1,
+		const int jpeg_lossless = -1) = 0;
+
+	virtual BYTE* __stdcall FileToRGB(const CString& FileName,
+		const int abs_size_x = 0, const int abs_size_y = 0,
+		const int rel_size_z = 0, const int rel_size_n = 0,
+		const enum scaling_type picture_scaling_type = scaling_type::scaling_type_none,
+		const bool b_scan = true) = 0;
+
+	virtual void __stdcall get_size(const CString& FileName) = 0;
+	virtual CString __stdcall get_info(const CString& FileName, const enum info_type _info_type);
+	virtual unsigned int __stdcall get_cap() const;
+};
+
+class CTiffFormat : public CPluginFormat
+{
+public:
+	CTiffFormat() {};
+
+public:
+	virtual ~CTiffFormat() {};
+
+protected:
+	static CString m_property_str;
+	static int m_compression_type;
+
+public:
+	virtual void __stdcall set_properties(const CString& property_str);
+	virtual CString __stdcall get_properties() const;
+	virtual bool __stdcall properties_dlg(const HWND hwnd);
+
+public:
+	static const CString type;
+	virtual const CString __stdcall getType() const
+	{
+		return type;
+	}
+
+	static CPictureFormat* __stdcall GetInstance()
+	{
+		return new CTiffFormat;
+	};
+
+	virtual bool __stdcall RGBToFile(const CString& FileName,
+		const BYTE* dataBuf,
+		const int width,
+		const int height,
+		const int quality_L = -1,
+		const int quality_C = -1,
+		const int jpeg_lossless = -1);
+
+	virtual BYTE* __stdcall FileToRGB(const CString& FileName,
+		const int abs_size_x = 0, const int abs_size_y = 0,
+		const int rel_size_z = 0, const int rel_size_n = 0,
+		const enum scaling_type picture_scaling_type = scaling_type::scaling_type_none,
+		const bool b_scan = true);
+
+	virtual void __stdcall get_size(const CString& FileName);
+	virtual CString __stdcall get_ext() const;
+	virtual struct plugin_data __stdcall get_plugin_data() const;
+};
+
+
+class CPngFormat : public CPluginFormat
+{
+public:
+	CPngFormat() {};
+
+public:
+	virtual ~CPngFormat() {};
+
+public:
+	static const CString type;
+	virtual const CString __stdcall getType() const
+	{
+		return type;
+	}
+
+	static CPictureFormat* __stdcall GetInstance()
+	{
+		return new CPngFormat;
+	};
+
+	virtual bool __stdcall RGBToFile(const CString& FileName,
+		const BYTE* dataBuf,
+		const int width,
+		const int height,
+		const int quality_L = -1,
+		const int quality_C = -1,
+		const int jpeg_lossless = -1);
+
+	//virtual BYTE* __stdcall FileToPreview(const CString& FileName, int& len, int& size_x, int& size_y, const bool bScanAudio = false, const bool bMaxSize = false);
+
+	virtual BYTE* __stdcall FileToRGB(const CString& FileName,
+		const int abs_size_x = 0, const int abs_size_y = 0,
+		const int rel_size_z = 0, const int rel_size_n = 0,
+		const enum scaling_type picture_scaling_type = scaling_type::scaling_type_none,
+		const bool b_scan = true);
+
+	virtual void __stdcall get_size(const CString& FileName);
+	virtual CString __stdcall get_ext() const;
+	virtual struct plugin_data __stdcall get_plugin_data() const;
+	virtual bool __stdcall properties_dlg(const HWND hwnd);
+};
---
+++ tiff_png_format/libtiff/cmake_install.cmake
@@ -0,0 +1,123 @@
+# Install script for directory: C:/Projects/libtiff/libtiff
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/tiff")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "Release")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "FALSE")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  if(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY OPTIONAL FILES "C:/Projects/libtiff/libtiff/Debug/tiffd.lib")
+  elseif(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY OPTIONAL FILES "C:/Projects/libtiff/libtiff/Release/tiff.lib")
+  elseif(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Mm][Ii][Nn][Ss][Ii][Zz][Ee][Rr][Ee][Ll])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY OPTIONAL FILES "C:/Projects/libtiff/libtiff/MinSizeRel/tiff.lib")
+  elseif(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Rr][Ee][Ll][Ww][Ii][Tt][Hh][Dd][Ee][Bb][Ii][Nn][Ff][Oo])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY OPTIONAL FILES "C:/Projects/libtiff/libtiff/RelWithDebInfo/tiff.lib")
+  endif()
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  if(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE SHARED_LIBRARY FILES "C:/Projects/libtiff/libtiff/Debug/tiffd.dll")
+  elseif(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE SHARED_LIBRARY FILES "C:/Projects/libtiff/libtiff/Release/tiff.dll")
+  elseif(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Mm][Ii][Nn][Ss][Ii][Zz][Ee][Rr][Ee][Ll])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE SHARED_LIBRARY FILES "C:/Projects/libtiff/libtiff/MinSizeRel/tiff.dll")
+  elseif(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Rr][Ee][Ll][Ww][Ii][Tt][Hh][Dd][Ee][Bb][Ii][Nn][Ff][Oo])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE SHARED_LIBRARY FILES "C:/Projects/libtiff/libtiff/RelWithDebInfo/tiff.dll")
+  endif()
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include" TYPE FILE FILES
+    "C:/Projects/libtiff/libtiff/tiff.h"
+    "C:/Projects/libtiff/libtiff/tiffio.h"
+    "C:/Projects/libtiff/libtiff/tiffvers.h"
+    "C:/Projects/libtiff/libtiff/tiffconf.h"
+    )
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/cmake/tiff/TiffTargets.cmake")
+    file(DIFFERENT _cmake_export_file_changed FILES
+         "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/cmake/tiff/TiffTargets.cmake"
+         "C:/Projects/libtiff/libtiff/CMakeFiles/Export/cc2b3a547891f5fbeec431910dcae38b/TiffTargets.cmake")
+    if(_cmake_export_file_changed)
+      file(GLOB _cmake_old_config_files "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/cmake/tiff/TiffTargets-*.cmake")
+      if(_cmake_old_config_files)
+        string(REPLACE ";" ", " _cmake_old_config_files_text "${_cmake_old_config_files}")
+        message(STATUS "Old export file \"$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/cmake/tiff/TiffTargets.cmake\" will be replaced.  Removing files [${_cmake_old_config_files_text}].")
+        unset(_cmake_old_config_files_text)
+        file(REMOVE ${_cmake_old_config_files})
+      endif()
+      unset(_cmake_old_config_files)
+    endif()
+    unset(_cmake_export_file_changed)
+  endif()
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/cmake/tiff" TYPE FILE FILES "C:/Projects/libtiff/libtiff/CMakeFiles/Export/cc2b3a547891f5fbeec431910dcae38b/TiffTargets.cmake")
+  if(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/cmake/tiff" TYPE FILE FILES "C:/Projects/libtiff/libtiff/CMakeFiles/Export/cc2b3a547891f5fbeec431910dcae38b/TiffTargets-debug.cmake")
+  endif()
+  if(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Mm][Ii][Nn][Ss][Ii][Zz][Ee][Rr][Ee][Ll])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/cmake/tiff" TYPE FILE FILES "C:/Projects/libtiff/libtiff/CMakeFiles/Export/cc2b3a547891f5fbeec431910dcae38b/TiffTargets-minsizerel.cmake")
+  endif()
+  if(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Rr][Ee][Ll][Ww][Ii][Tt][Hh][Dd][Ee][Bb][Ii][Nn][Ff][Oo])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/cmake/tiff" TYPE FILE FILES "C:/Projects/libtiff/libtiff/CMakeFiles/Export/cc2b3a547891f5fbeec431910dcae38b/TiffTargets-relwithdebinfo.cmake")
+  endif()
+  if(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/cmake/tiff" TYPE FILE FILES "C:/Projects/libtiff/libtiff/CMakeFiles/Export/cc2b3a547891f5fbeec431910dcae38b/TiffTargets-release.cmake")
+  endif()
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/cmake/tiff" TYPE FILE FILES "C:/Projects/libtiff/libtiff/TiffConfigVersion.cmake")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/cmake/tiff" TYPE FILE FILES "C:/Projects/libtiff/libtiff/TiffConfig.cmake")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  if(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "C:/Projects/libtiff/libtiff/Debug/tiffxxd.lib")
+  elseif(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "C:/Projects/libtiff/libtiff/Release/tiffxx.lib")
+  elseif(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Mm][Ii][Nn][Ss][Ii][Zz][Ee][Rr][Ee][Ll])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "C:/Projects/libtiff/libtiff/MinSizeRel/tiffxx.lib")
+  elseif(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Rr][Ee][Ll][Ww][Ii][Tt][Hh][Dd][Ee][Bb][Ii][Nn][Ff][Oo])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "C:/Projects/libtiff/libtiff/RelWithDebInfo/tiffxx.lib")
+  endif()
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include" TYPE FILE FILES "C:/Projects/libtiff/libtiff/tiffio.hxx")
+endif()
+
